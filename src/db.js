f655a43c80188c04f839198d392c0cecf4f6ff0d
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-04-03 18:19:08 +0530

    First Commit

---
+++ src/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const DB_URL =
+  process.env.DB ||
+  "mongodb+srv://trexuser:trexpassword@trextesting.ixnokth.mongodb.net/ffish";
+
+const ConnectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(DB_URL);
+    console.log(`Connected to MongoDB: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection FAILED", error);
+    process.exit(1);
+  }
+};
+
+export default ConnectDB;
